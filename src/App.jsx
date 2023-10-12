20644362e9cf03d30e0137313305f0c88876f10b
Your Nameeli <elighidiu@yahoo.com>
2023-10-12 12:34:46 +0200

    remove tour

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
   const fetchTours = async () => {
     setIsLoading(true);
     try {
@@ -35,7 +39,7 @@ function App() {
   }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
