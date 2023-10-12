07ee1a7605019a47e6f07a276987b2cea99dd618
Your Nameeli <elighidiu@yahoo.com>
2023-10-12 13:34:08 +0200

    build

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,24 @@ function App() {
       </main>
     );
   }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button
+            type="button"
+            style={{ marginTop: "2rem" }}
+            className="btn"
+            onClick={() => fetchTours()}
+          >
+            Reload tours
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
